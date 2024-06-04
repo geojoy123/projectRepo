8d512bd341c84ce4931c6988cb01088c92b0f7c8
geojoy123 <geojoy861997@gmail.com>
2024-06-04 15:12:08 +0530

    new function add in BMI.java
    vnb

---
+++ src/test/java/pojectBmiautomationsectionone/registerationsession.java
@@ -0,0 +1,12 @@
+package pojectBmiautomationsectionone;
+
+public class registerationsession {
+
+	
+	//this is the text seesion of the prokject
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
